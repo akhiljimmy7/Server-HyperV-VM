84f1a1530c22ef5b56008dbea970cc6d5518ef5c
akhiljimmy7 <40021949+akhiljimmy7@users.noreply.github.com>
2020-08-09 14:13:18 +0800

    Update ChangeVMHostnameIP.ps1
    Adding comments

--- ChangeVMHostnameIP.ps1
+++ ChangeVMHostnameIP.ps1
@@ -1,4 +1,4 @@
-﻿$csv3 = Import-Csv "C:\Scripting\ENG01\VMDetails.csv" -Header "SrcIPaddress","Hostname","TgtIPAddress","SubnetMask","DefaultGateway" -Delimiter ","
+﻿$csv3 = Import-Csv "C:\location\item\VMDetails.csv" -Header "SrcIPaddress","Hostname","TgtIPAddress","SubnetMask","DefaultGateway" -Delimiter ","
 #Clear-Item -Path WSMan:\localhost\Client\TrustedHosts -Force
 $counter = 0
 
@@ -6,23 +6,13 @@ $counter = 0
 
 foreach($item in $csv3)
 {
-
- 
-
 if ($counter -gt 0)
 {
 
- 
-
-#uncomment this line to add new IP address to Trusted Host List if host no added to list
-Set-Item WSMan:\localhost\Client\TrustedHosts -Value $item.SrcIPaddress -Concatenate
+#uncomment this line to add new IP address to Trusted Host List if host no added to list, forced 
+Set-Item WSMan:\localhost\Client\TrustedHosts -Value $item.SrcIPaddress -Concatenate -force
 Write-Host $item.SrcIPaddress,$item.TgtIPaddress,$item.Hostname,$item.SubnetMask,$item.DefaultGateway
 
- 
-
- 
-
- 
 
 Invoke-Command -ComputerName $item.SrcIPaddress -AsJob -ScriptBlock {
 
@@ -31,48 +21,33 @@ Invoke-Command -ComputerName $item.SrcIPaddress -AsJob -ScriptBlock {
 Param (
 $item )
 
- 
-
+#Renaming VM Hostname
 Rename-Computer -NewName $item.Hostname
-
- 
 
 Write-Host $item.SrcIPaddress,$item.TgtIPaddress,$item.Hostname,$item.SubnetMask,$item.DefaultGateway
 
  
-
+#Assuming 2 VMs and VM NIC names are Ethernet and Ethernet 3
 netsh interface ip set address "Ethernet" static $item.TgtIPAddress $item.SubnetMask $item.DefaultGateway
-netsh interface ip set dns "Ethernet" static 172.16.0.220
-netsh interface ip add dns "Ethernet" 172.16.0.14
+netsh interface ip set dns "Ethernet" static 172.16.0.10
+netsh interface ip add dns "Ethernet" 172.16.0.20
 netsh interface ip set wins name="Ethernet" source=static addr=172.16.0.30
 
 
 netsh interface ip set address "Ethernet 3" static $item.TgtIPAddress $item.SubnetMask $item.DefaultGateway
-netsh interface ip set dns "Ethernet 3" 172.16.0.220
-netsh interface ip add dns "Ethernet 3" 172.16.0.14
-netsh interface ip set wins name="Ethernet 3" source=static addr=172.16.0.30
+netsh interface ip set dns "Ethernet 3" 172.16.0.10
+netsh interface ip add dns "Ethernet 3" 172.16.0.20
+netsh interface ip set wins name="Ethernet 3" source=static addr=172.16.0.10
 
- 
-
-
+#Disabling IPv6
 Disable-NetAdapterBinding -Name "Ethernet" -ComponentID ms_tcpip6
 Disable-NetAdapterBinding -Name "Ethernet 3" -ComponentID ms_tcpip6
- 
-
- 
 
 #restart server
 Restart-Computer -Force
 
- 
-
- 
 
 } -ArgumentList $item
-
- 
-
- 
 
 }
 $counter++
