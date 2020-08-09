2b2f7d923c20d6811748933b279df1934269ca83
akhiljimmy7 <40021949+akhiljimmy7@users.noreply.github.com>
2020-08-09 16:05:02 +0800

    Update CreateVSwitch.ps1

--- CreateVSwitch.ps1
+++ CreateVSwitch.ps1
@@ -1,4 +1,4 @@
-﻿$csv3 = Import-Csv "C:\Scripting\ENG01\HostNameIP_CreateVSwitch.csv" -Header "SrcIPaddress","Hostname" -Delimiter ","
+﻿$csv3 = Import-Csv "C:\location\item\HostNameIP_CreateVSwitch.csv" -Header "SrcIPaddress","Hostname" -Delimiter ","
 #Clear-Item -Path WSMan:\localhost\Client\TrustedHosts -Force
 $counter = 0
  
@@ -12,26 +12,21 @@ if ($counter -gt 0)
 {
 
 #uncomment this line to add new IP address to Trusted Host List if host no added to list
-Set-Item WSMan:\localhost\Client\TrustedHosts -Value $item.SrcIPaddress -Concatenate
+Set-Item WSMan:\localhost\Client\TrustedHosts -Value $item.SrcIPaddress -Concatenate -force
 Write-Host $item.SrcIPaddress,$item.Hostname
 Invoke-Command -ComputerName $item.SrcIPaddress -AsJob -ScriptBlock {
 
 Param (
 $item ) 
 
-New-VMSwitch -name "New Virtual Switch" -NetAdapterName "Embedded FlexibleLOM 1 Port 1 2" -AllowManagementOS $true
+New-VMSwitch -name "New Virtual Switch" -NetAdapterName "Network Adapter Name" -AllowManagementOS $true
 #Restart Blade Host or the computer-VM
 
-
- 
-
- 
+Start-Sleep -s 15
+Start-VM nameofvm*
 
 } -ArgumentList $item
 
- 
-
- 
 
 }
 $counter++
