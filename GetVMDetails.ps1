4f0f0dbe1bfdba75143af0e793c72e133e57b256
akhiljimmy7 <40021949+akhiljimmy7@users.noreply.github.com>
2020-08-09 16:06:06 +0800

    Update GetVMDetails.ps1

--- GetVMDetails.ps1
+++ GetVMDetails.ps1
@@ -1,11 +1,9 @@
 ﻿$Output= @()
-    $names = Get-Content "C:\Scripting\ENG01\HostNames.txt"
-    #$command1=get-vm -ComputerName $name | ?{$_.State -eq "Running"} | select -ExpandProperty networkadapters | select vmname, macaddress, switchname, ipaddresses, computername
+    $names = Get-Content "C:\location\item\HostNames.txt"
+    
     foreach ($name in $names){
-   # Invoke-Command -ComputerName $name -AsJob -ScriptBlock {
-   # $out1=get-vmnetworkadapter -All -ComputerName $name | Select ipaddresses
      get-vm -ComputerName $name | ?{$_.State -eq "Running"} | select -ExpandProperty networkadapters | select vmname, macaddress, switchname, ipaddresses, computername, name
-    # Get-VMNetworkAdapter -ComputerName $name | select name 
+    
     $Output+= "$command1"
     }
    # }
