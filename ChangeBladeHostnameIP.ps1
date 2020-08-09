b90b307910ea0d75db60b9798927230fe371f8bc
akhiljimmy7 <40021949+akhiljimmy7@users.noreply.github.com>
2020-08-09 14:16:10 +0800

    Update ChangeBladeHostnameIP.ps1

--- ChangeBladeHostnameIP.ps1
+++ ChangeBladeHostnameIP.ps1
@@ -1,4 +1,4 @@
-﻿$csv3 = Import-Csv "C:\Scripting\ENG01\HostDetails.csv" -Header "SrcIPaddress","Hostname","TgtIPAddress","SubnetMask","DefaultGateway" -Delimiter ","
+﻿$csv3 = Import-Csv "C:\location\item\HostDetails.csv" -Header "SrcIPaddress","Hostname","TgtIPAddress","SubnetMask","DefaultGateway" -Delimiter ","
 #Clear-Item -Path WSMan:\localhost\Client\TrustedHosts -Force
 $counter = 0
 foreach($item in $csv3)
@@ -6,20 +6,23 @@ foreach($item in $csv3)
 if ($counter -gt 0)
 {
 #uncomment this line to add new IP address to Trusted Host List if host no added to list
-Set-Item WSMan:\localhost\Client\TrustedHosts -Value $item.SrcIPaddress -Concatenate
+Set-Item WSMan:\localhost\Client\TrustedHosts -Value $item.SrcIPaddress -Concatenate -force
 Write-Host $item.SrcIPaddress,$item.TgtIPaddress,$item.Hostname,$item.SubnetMask,$item.DefaultGateway
 Invoke-Command -ComputerName $item.SrcIPaddress -AsJob -ScriptBlock {
 Param (
 $item )
-#Rename-Computer -NewName $item.Hostname
+#Renaming Computer Hostname
+Rename-Computer -NewName $item.Hostname
+
 Write-Host $item.SrcIPaddress,$item.TgtIPaddress,$item.Hostname,$item.SubnetMask,$item.DefaultGateway
-netsh interface ip set address "Embedded FlexibleLOM 1 Port 1 2" static $item.TgtIPAddress $item.SubnetMask $item.DefaultGateway
-netsh interface ip set dns "Embedded FlexibleLOM 1 Port 1 2" static 172.16.0.220
-netsh interface ip add dns "Embedded FlexibleLOM 1 Port 1 2" 172.16.0.14
-netsh interface ip set wins name="Embedded FlexibleLOM 1 Port 1 2" source=static addr=172.16.0.30
-Disable-NetAdapterBinding -Name "Embedded FlexibleLOM 1 Port 1 2" -ComponentID ms_tcpip6
+netsh interface ip set address "Network Adapter Name" static $item.TgtIPAddress $item.SubnetMask $item.DefaultGateway
+netsh interface ip set dns "Network Adapter Name" static 172.16.0.10
+netsh interface ip add dns "Network Adapter Name" 172.16.0.20
+netsh interface ip set wins name="Network Adapter Name" source=static addr=172.16.0.30
+Disable-NetAdapterBinding -Name "Network Adapter Name" -ComponentID ms_tcpip6
 #restart server
-#Restart-Computer -Force
+
+Restart-Computer -Force
 } -ArgumentList $item
 }
 $counter++
